b3f4f681610524a20310b4a09ae033eea69745a7
Elirudite <elijahclaude@gmail.com>
2016-06-08 12:29:57 -0400

    Added a way to hide and count completed tasks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,31 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
+import './task.js';
 import './body.html';
+
+Template.body.onCreated(function bodyOnCreated() {
+	this.state = new ReactiveDict();
+});
 
 Template.body.helpers({
 	tasks() {
+		const instance = Template.instance();
+		if (instance.state.get('hideCompleted')) {
+		// completedTasksCount() {
+		// 	return Tasks.find({ checked: { $ne: true } }).count();
+		// }
+			// [If] hide completed is checked, [do] filter tasks
+			return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1} });
+		}
+		// Otherwise [else], return all of the tasks
 		// Shows newest tassks at the top
 		return Tasks.find({}, { sort: { createdAt: -1 } });
+	},
+	incompleteTasksCount() {
+		return Tasks.find({ checked: { $ne: true } }).count();
 	},
 });
 
@@ -28,5 +46,8 @@ Template.body.events({
 
 		// Clears form
 		target.text.value = '';
+	},
+	'change .hide-completed input' (event, instance) {
+		instance.state.set('hideCompleted', event.target.checked);
 	},
 });
