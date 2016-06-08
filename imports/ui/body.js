42841741bf54e656e82a99d1936db90f6afeb612
Elirudite <elijahclaude@gmail.com>
2016-06-08 11:44:02 -0400

    Added input form and newest-to-oldest sorting

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,6 +6,27 @@ import './body.html';
 
 Template.body.helpers({
 	tasks() {
-		return Tasks.find({});
+		// Shows newest tassks at the top
+		return Tasks.find({}, { sort: { createdAt: -1 } });
+	},
+});
+
+Template.body.events({
+	'submit .new-task' (event) {
+		// Prevents default browser from submit
+		event.preventDefault();
+
+		// Gets the value from form element
+		const target = event.target
+		const text = target.text.value;
+
+		// Inserts a task into the collection
+		Tasks.insert({
+			text,
+			createdAt: new Date(), // current time
+		});
+
+		// Clears form
+		target.text.value = '';
 	},
 });
